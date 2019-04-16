8ff7cbe457b00479aa32c6f6b2dd139970e6a29f
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-16 11:09:02 -0300

    Create reducers index file and export them

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
