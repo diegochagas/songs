400a624b668ad1b2a33443bfafc29b8bd13d6214
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-17 10:57:08 -0300

    Show song details

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SongDetails = () => {
+const SongDetails = props => {
+  if(!props.selectedSong){
+    return <div>Select song</div>;
+  }
+
   return(
-    <div>SongDetail</div>
+    <div>
+      <h1>Details for:</h1>
+      <p>Title: {props.selectedSong.title}</p>
+      <p>Length: {props.selectedSong.duration}</p>
+    </div>
   );
 }
 
-export default SongDetails;
+const mapStateToProps = state => {
+  return {selectedSong: state.selectedSong}
+}
+
+export default connect(mapStateToProps)(SongDetails);
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   render(){
     return(
-      <div>SongList</div>
+      <div>
+        <h3>Song List</h3>
+        <ul>
+          {this.renderSongs()}
+        </ul>
+      </div>
     );
+  }
+  
+  renderSongs(){
+    return this.props.songs.map(song => {
+      return (
+        <li key={song.title}>
+          <p>Title: {song.title}, duration: {song.duration}</p>
+          <button onClick={() => this.props.selectSong(song)}>Select</button>
+        </li>
+      )
+    });
   }
 }
 
 const mapStateToProps = state => {
-  return { songs: state.songs};
+  return { songs: state.songs, selectSong: state.selectSong};
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {selectSong})(SongList);
